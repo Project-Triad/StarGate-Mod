3945b80355fbb38e05b568db5b92b2b74cbc36d0
Aladalar <aladalar@gmail.com>
2016-10-02 15:16:44 +0200

    First changes

---
+++ src/main/java/cz/projectTriad/stargate/PTSG.java
@@ -0,0 +1,12 @@
+package cz.projectTriad.stargate;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = PTSG.MODID, name = PTSG.MODNAME, version = PTSG.VERSION)
+public class PTSG {
+	
+	public static final String MODID = "ProjT_SGC";
+	public static final String MODNAME = "ProjectTriad - StarGateCore";
+	public static final String VERSION = "0.0.1";
+
+}
