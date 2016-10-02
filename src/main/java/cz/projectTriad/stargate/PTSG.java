44bf8d73b7541ba1b9d3d11a268dbbed47765ccb
Aladalar <aladalar@gmail.com>
2016-10-02 15:49:35 +0200

    Code 1
    Finished Main-class, Sided proxy

--- src/main/java/cz/projectTriad/stargate/PTSG.java
+++ src/main/java/cz/projectTriad/stargate/PTSG.java
@@ -1,6 +1,13 @@
 package cz.projectTriad.stargate;
 
+import cz.projectTriad.stargate.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = PTSG.MODID, name = PTSG.MODNAME, version = PTSG.VERSION)
 public class PTSG {
@@ -9,4 +16,25 @@ public class PTSG {
 	public static final String MODNAME = "ProjectTriad - StarGateCore";
 	public static final String VERSION = "0.0.1";
 
+	@SidedProxy(clientSide="cz.projectTriad.stargate.proxy.ClientProxy", serverSide="cz.projectTriad.stargate.proxy.ServerProxy")
+	public static CommonProxy proxy;
+	
+	@Instance
+	public static PTSG instace = new PTSG();
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) 
+	{
+		
+	}
+	@EventHandler
+	public void Init(FMLInitializationEvent e) 
+	{
+		
+	}
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent e) 
+	{
+		
+	}
 }
